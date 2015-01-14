d88b79778c7c04fdcedd67770f55178cbf0da433
SamuraiSusy <susyy-@hotmail.com>
2015-01-14 14:18:00 +0200

    switch between attack and monster menus "work"

--- Assets/Scripts/BattleMenu.cs
+++ Assets/Scripts/BattleMenu.cs
@@ -115,18 +115,27 @@ public class BattleMenu : MonoBehaviour
 
     void DrawItemMenu()
     {
-        //cam1.enabled = !cam2.enabled;
-        cam2.enabled = !cam3.enabled;
-        cam3.enabled = !cam1.enabled;
+        //cam1.enabled = false;
+        //cam2.enabled = true;
+        //cam3.enabled = false;
+        //Debug.Log("drawmenuitem");
     }
 
     // don't quite get it but it changes the cameras wrong way
     // still usable, only little, fixable bug
     void DrawMonsterMenu()
     {
-        cam1.enabled = !cam1.enabled;
-        //cam2.enabled = !cam2.enabled;
-        cam3.enabled = !cam3.enabled;
+        if (cam1.enabled == true)
+        {
+            cam1.enabled = false;
+            cam2.enabled = false;
+            cam3.enabled = true;
+        }
+        else if (cam3.enabled == true && cam1.enabled == false)
+        {
+            cam1.enabled = true;
+            cam3.enabled = false;
+        }
     }
 
     void Attack()
